25f696cea4ff51eab0b69884d50f9fc844ea54d2
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2022-03-01 17:13:05 +0800

    Updated redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 const path = require('path')
 module.exports = {
+	async redirects() {
+		return [
+		  {
+			source: '/category',
+			destination: '/category/all',
+			permanent: true,
+		  },
+		  {
+			source: '/product',
+			destination: '/category/all',
+			permanent: true,
+		  },
+		]
+	},
 	trailingSlash: false,
 	webpackDevMiddleware: config => {
 		config.watchOptions = {
